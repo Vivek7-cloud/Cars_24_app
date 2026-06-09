51f51f1ae5ed50e792d26b9574a06c8dd4dce3b0
Vivek7-cloud <vivek.kr.8235@outlook.com>
2026-06-10 00:04:29 +0530

    Calc formula

---
+++ Demo/calc.py
@@ -0,0 +1,12 @@
+def addition(a,b,c,d,e):
+    return a+b+c+d+e
+
+def subtraction(a,b,c,d,e):
+    if a>b and a>c and a>d and a>e:
+        return a-b-c-d-e
+    elif b>a and b>c and b>d and b>e:
+        return b-a-c-d-e
+    elif c>a and c>b and c>d and c>e:
+        return c-a-b-d-e
+    elif d>a and d>b and d>c and d>e:
+        
